492b7a766d59fa0dcd6467514e46f081e0914a21
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 16:00:58 +0100

    feat: add markup

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,7 +3,28 @@ import './Featured.scss';
 const Featured = () => {
   return (
     <section className='featured'>
-      <div className='container'>Featured</div>
+      <div className='container'>
+        <div className='left'>
+          <h1>Find the perfect freelance services for your business</h1>
+          <div className='search'>
+            <div className='searchInput'>
+              <img src='/img/search.png' alt='search icon' />
+              <input type='text' placeholder='Try "building mobile app"' />
+            </div>
+            <button>Search</button>
+          </div>
+          <div className='popular'>
+            <span>Popular:</span>
+            <button>Web Design</button>
+            <button>WordPress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
+          </div>
+        </div>
+        <div className='right'>
+          <img src='/img/man.png' alt='man' />
+        </div>
+      </div>
     </section>
   );
 };
