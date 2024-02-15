d2739c640e70b2a2acc61f256b2ae714d8dd44e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 17:32:20 +0100

    feat: add markup

--- src/components/categoryCard/CategoryCard.jsx
+++ src/components/categoryCard/CategoryCard.jsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
+
 import './CategoryCard.scss';
 
-const CategoryCard = () => {
-  return <div className='categoryCard'>CategoryCard</div>;
+const CategoryCard = ({ img, desc, title }) => {
+  return (
+    <Link to='/gig?category=design'>
+      <article className='categoryCard'>
+        <img src={img} alt={title} />
+        <span className='desc'>{desc}</span>
+        <span className='title'>{title}</span>
+      </article>
+    </Link>
+  );
 };
 
 export default CategoryCard;
