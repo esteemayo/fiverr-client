685e356ab737031f99244c5dbdf6cd7da1d7513f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 16:49:07 +0100

    feat: add markup

--- src/components/trustedBy/TrustedBy.jsx
+++ src/components/trustedBy/TrustedBy.jsx
@@ -1,7 +1,31 @@
 import './TrustedBy.scss';
 
 const TrustedBy = () => {
-  return <section className='trustedBy'>TrustedBy</section>;
+  return (
+    <section className='trustedBy'>
+      <span>Trusted by:</span>
+      <img
+        src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook2x.188a797.png'
+        alt='image'
+      />
+      <img
+        src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google2x.06d74c8.png'
+        alt='image'
+      />
+      <img
+        src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/nextflix2x.887e47e.png'
+        alt='image'
+      />
+      <img
+        src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg2x.6dc32e4.png'
+        alt='image'
+      />
+      <img
+        src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal2x.22728be.png'
+        alt='image'
+      />
+    </section>
+  );
 };
 
 export default TrustedBy;
