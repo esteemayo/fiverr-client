b77a1969681310aaffc61eb162fc266f6525cc42
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 16:10:23 +0100

    feat: add styling

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,7 +5,9 @@ const Featured = () => {
     <section className='featured'>
       <div className='container'>
         <div className='left'>
-          <h1>Find the perfect freelance services for your business</h1>
+          <h1>
+            Find the perfect <i>freelance</i> services for your business
+          </h1>
           <div className='search'>
             <div className='searchInput'>
               <img src='/img/search.png' alt='search icon' />
