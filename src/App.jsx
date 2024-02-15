c75db30fb62f4870e5cde80670be06fd8700d581
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 15:58:26 +0100

    fix: modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,77 @@
-import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { router } from './utils/route';
+import MyGigs from './pages/myGigs/MyGigs';
+import Add from './pages/add/Add';
+import Message from './pages/message/Message';
+import Gig from './pages/gig/Gig';
+import Register from './pages/register/Register';
+import Home from './pages/home/Home';
+import Messages from './pages/messages/Messages';
+import Gigs from './pages/gigs/Gigs';
+import NotFound from './pages/notFound/NotFound';
+import Login from './pages/login/Login';
+import Orders from './pages/register/orders/Orders';
+
+import SharedLayout from './components/sharedLayout/SharedLayout';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <SharedLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: 'gigs',
+          element: <Gigs />,
+        },
+        {
+          path: 'gig/:id',
+          element: <Gig />,
+        },
+        {
+          path: 'orders',
+          element: <Orders />,
+        },
+        {
+          path: 'mygigs',
+          element: <MyGigs />,
+        },
+        {
+          path: 'add',
+          element: <Add />,
+        },
+        {
+          path: 'messages',
+          element: <Messages />,
+        },
+        {
+          path: 'message/:id',
+          element: <Message />,
+        },
+        {
+          path: 'login',
+          element: <Login />,
+        },
+        {
+          path: 'register',
+          element: <Register />,
+        },
+        {
+          path: '',
+          element: <Home />,
+        },
+        {
+          path: '*',
+          element: <NotFound />,
+        },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
