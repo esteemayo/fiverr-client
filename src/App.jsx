db11fccd64b90721c0169f6aa39e84e2e722ce2a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 15:56:20 +0100

    fix: rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 
-import { router } from './utils/router';
+import { router } from './utils/route';
 
 function App() {
   return <RouterProvider router={router} />;
