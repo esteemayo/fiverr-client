5e2d3daf95a215cf8ae06424f88fcf125f6dd1e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 02:17:23 +0100

    feat: add markup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,22 @@
 const Navbar = () => {
-  return <nav className='navbar'>Navbar</nav>;
+  return (
+    <nav className='navbar'>
+      <div className='container'>
+        <div className='logo'>
+          <span className='text'>fiverr</span>
+          <span className='dot'>.</span>
+        </div>
+        <div className='links'>
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
