8996f177ef116e3d03277b96b2e3e0e9e8f212ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-15 16:43:24 +0100

    feat: add TrustedBy component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
 
 import './Home.scss';
 
 const Home = () => {
   return <main className='home'>
     <Featured />
+    <TrustedBy />
   </main>;
 };
 
