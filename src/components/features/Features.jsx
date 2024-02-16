cf9d9459ed5f30aa6b8aa15b54e6a7635095e9bb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-16 02:06:13 +0100

    fix: modify

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -8,34 +8,34 @@ const Features = () => {
           <h1>A whole world of freelance talent at your fingertips</h1>
           <div className='title'>
             <img src='/img/check.png' alt='' />
-            <span>The best for every budget</span>
+            <h4>The best for every budget</h4>
           </div>
           <p>
-            Find hight-quality services at every price point. No hourly rates,
+            Find high-quality services at every price point. No hourly rates,
             just project-based pricing.
           </p>
           <div className='title'>
             <img src='/img/check.png' alt='' />
-            <span>The best for every budget</span>
+            <h4>Quality work done quickly</h4>
           </div>
           <p>
-            Find hight-quality services at every price point. No hourly rates,
-            just project-based pricing.
+            Find the right freelancer to begin working on your project within
+            minutes.
           </p>
           <div className='title'>
             <img src='/img/check.png' alt='' />
-            <span>The best for every budget</span>
+            <h4>Protected payments, every time</h4>
           </div>
           <p>
-            Find hight-quality services at every price point. No hourly rates,
-            just project-based pricing.
+            Always know what you'll pay upfront. Your payment isn't released
+            until you approve the work.
           </p>
           <div className='title'>
             <img src='/img/check.png' alt='' />
-            <span>The best for every budget</span>
+            <h4>24/7 support</h4>
           </div>
           <p>
-            Find hight-quality services at every price point. No hourly rates,
+            Find high-quality services at every price point. No hourly rates,
             just project-based pricing.
           </p>
         </div>
