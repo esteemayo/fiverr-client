06e0bfd1d2149e9e3063ac43e09bfa43b5ada45e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-16 02:57:30 +0100

    feat: add ProjectCard component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,11 @@ import Business from '../../components/business/Business';
 import Slide from '../../components/slide/Slide';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Featured from '../../components/featured/Featured';
-import CategoryCard from '../../components/categoryCard/CategoryCard';
+import ProjectCard from '../../components/projectCard/ProjectCard';
 import Features from '../../components/features/Features';
+import CategoryCard from '../../components/categoryCard/CategoryCard';
 
-import { cards } from '../../data';
+import { cards, projects } from '../../data';
 
 import './Home.scss';
 
@@ -20,6 +21,11 @@ const Home = () => {
     </Slide>
     <Features />
     <Business />
+    <Slide arrowsScroll={5} slidesToShow={5} >
+      {projects.map((item) => {
+        return <ProjectCard key={item.id} {...item} />;
+      })}
+    </Slide>
   </main>;
 };
 
