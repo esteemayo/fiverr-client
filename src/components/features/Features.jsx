d0abb9c041fd7ca414d292a4309d16f2b5bb62ae
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-16 01:35:57 +0100

    feat: add markup

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,7 +1,50 @@
 import './Features.scss';
 
 const Features = () => {
-  return <section className='features'>Features</section>;
+  return (
+    <section className='features'>
+      <div className='container'>
+        <div className='item'>
+          <h1>A whole world of freelance talent at your fingertips</h1>
+          <div className='title'>
+            <img src='/img/check.png' alt='' />
+            <span>The best for every budget</span>
+          </div>
+          <p>
+            Find hight-quality services at every price point. No hourly rates,
+            just project-based pricing.
+          </p>
+          <div className='title'>
+            <img src='/img/check.png' alt='' />
+            <span>The best for every budget</span>
+          </div>
+          <p>
+            Find hight-quality services at every price point. No hourly rates,
+            just project-based pricing.
+          </p>
+          <div className='title'>
+            <img src='/img/check.png' alt='' />
+            <span>The best for every budget</span>
+          </div>
+          <p>
+            Find hight-quality services at every price point. No hourly rates,
+            just project-based pricing.
+          </p>
+          <div className='title'>
+            <img src='/img/check.png' alt='' />
+            <span>The best for every budget</span>
+          </div>
+          <p>
+            Find hight-quality services at every price point. No hourly rates,
+            just project-based pricing.
+          </p>
+        </div>
+        <div className='item'>
+          <video src='img/video.mp4' controls></video>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
