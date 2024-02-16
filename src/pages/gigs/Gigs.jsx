b1ad24c6c9687d6cc66a7f65b6880eb5ffbd8256
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-16 17:52:30 +0100

    feat: handle sort menu

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,8 +1,26 @@
+import { useCallback, useMemo, useState } from 'react';
+
 import GigCard from '../../components/gigCard/GigCard';
 
 import './Gigs.scss';
 
 const Gigs = () => {
+  const [sort, setSort] = useState('sales');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  const handleSort = useCallback((type) => {
+    setSort(type);
+    setIsOpen(false);
+  }, []);
+
+  const sortLabel = useMemo(() => {
+    return sort === 'sales' ? 'Best Selling' : 'Newest';
+  }, [sort]);
+
   return (
     <main className='gigs'>
       <div className='container'>
@@ -21,12 +39,17 @@ const Gigs = () => {
           </div>
           <div className='right'>
             <span className='sortBy'>SortBy</span>
-            <span className='sortType'>Best Selling</span>
-            <img src='/img/down.png' alt='down icon' />
-            <div className='rightMenu'>
-              <span>Newest</span>
-              <span>Best Selling</span>
-            </div>
+            <span className='sortType'>{sortLabel}</span>
+            <img src='/img/down.png' alt='down icon' onClick={handleToggle} />
+            {isOpen && (
+              <div className='rightMenu'>
+                {sort === 'sales' ? (
+                  <span onClick={() => handleSort('createdAt')}>Newest</span>
+                ) : (
+                  <span onClick={() => handleSort('sales')}>Best Selling</span>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
