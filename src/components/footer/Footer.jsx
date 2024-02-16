1f5042e526c6ab4d5a95ccf70c8b542d33de6c37
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-16 03:42:24 +0100

    feat: add styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -83,6 +83,7 @@ const Footer = () => {
             <span>Sitemap</span>
           </div>
         </div>
+        <hr />
         <div className='bottom'>
           <div className='left'>
             <h2>fiverr</h2>
