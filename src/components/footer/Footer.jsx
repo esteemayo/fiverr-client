dc5cf9dd53cdd272e4fdf40049cdcdeea2e05943
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-16 03:32:22 +0100

    feat: add markup

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,115 @@
+import { useMemo } from 'react';
+
+import './Footer.scss';
+
 const Footer = () => {
-  return <footer className='footer'>Footer</footer>;
+  const year = useMemo(() => {
+    const date = new Date();
+    return date.getFullYear();
+  }, []);
+
+  return (
+    <footer className='footer'>
+      <div className='container'>
+        <div className='top'>
+          <div className='item'>
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className='item'>
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className='item'>
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className='item'>
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className='item'>
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='left'>
+            <h2>fiverr</h2>
+            <span>© Fiverr International Ltd. {year}</span>
+          </div>
+          <div className='right'>
+            <div className='social'>
+              <img src='/img/twitter.png' alt='twitter' />
+              <img src='/img/facebook.png' alt='facebook' />
+              <img src='/img/linkedin.png' alt='linkedin' />
+              <img src='/img/pinterest.png' alt='pinterest' />
+              <img src='/img/instagram.png' alt='instagram' />
+            </div>
+            <div className='link'>
+              <img src='/img/language.png' alt='language' />
+              <span>English</span>
+            </div>
+            <div className='link'>
+              <img src='/img/coin.png' alt='coin' />
+              <span>USD</span>
+            </div>
+            <img src='/img/accessibility.png' alt='accessibility' />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
