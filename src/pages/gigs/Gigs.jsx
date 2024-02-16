54cd27007299ed4b82fdb34ed7ce33d25d0ae771
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-16 05:05:57 +0100

    feat: add markup

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,7 +1,37 @@
+import GigCard from '../../components/gigCard/GigCard';
+
 import './Gigs.scss';
 
 const Gigs = () => {
-  return <div className='gigs'>Gigs</div>;
+  return (
+    <main className='gigs'>
+      <div className='container'>
+        <span className='breadcrumbs'>FIVERR &gt; GRAPHICS & DESIGN &gt;</span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Fiverr&apos;s AI
+          artists
+        </p>
+        <div className='menu'>
+          <div className='left'>
+            <span>Budget</span>
+            <input type='text' placeholder='min' />
+            <input type='text' placeholder='max' />
+            <button>Apply</button>
+          </div>
+          <div className='right'>
+            <span className='sortBy'>SortBy</span>
+            <span className='sortType'>Best Selling</span>
+            <img src='/img/down.png' alt='down icon' />
+            <div className='rightMenu'>
+              <span>Newest</span>
+              <span>Best Selling</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Gigs;
