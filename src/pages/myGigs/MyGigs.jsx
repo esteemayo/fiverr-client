df6c83be269a77334363c0fa532f10c4506cf5c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-17 22:51:29 +0100

    feat: add markup

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,7 +1,43 @@
+import { Link } from 'react-router-dom';
+
 import './MyGigs.scss';
 
 const MyGigs = () => {
-  return <main className='myGigs'>MyGigs</main>;
+  return (
+    <main className='myGigs'>
+      <div className='container'>
+        <div className='title'>
+          <h1>Gigs</h1>
+          <Link to='/add'>Add new gig</Link>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Orders</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <td>
+              <img
+                src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
+                alt='image'
+              />
+            </td>
+            <td>Gig 1</td>
+            <td>$88</td>
+            <td>123</td>
+            <td>
+              <img src='/img/delete.png' alt='delete icon' />
+            </td>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
 };
 
 export default MyGigs;
