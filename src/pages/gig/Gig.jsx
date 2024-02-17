2e98ea2b1e581454f6ac5a1692779fb60643003c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-17 04:39:45 +0100

    feat: add markup

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -243,7 +243,45 @@ const Gig = () => {
             <hr />
           </div>
         </div>
-        <div className='right'>Right</div>
+        <div className='right'>
+          <div className='price'>
+            <h3>1 AI generated image</h3>
+            <h2>$59.99</h2>
+          </div>
+          <p>
+            I will create a unique high quality AI generated image based on a
+            description that you give me
+          </p>
+          <div className='details'>
+            <div className='item'>
+              <img src='/img/clock.png' alt='clock icon' />
+              <span>2 days delivery</span>
+            </div>
+            <div className='item'>
+              <img src='/img/recyle.png' alt='recycle icon' />
+              <span>3 Revisions</span>
+            </div>
+          </div>
+          <div className='features'>
+            <div className='item'>
+              <img src='/img/greencheck.png' alt='greencheck icon' />
+              <span>Prompt writing</span>
+            </div>
+            <div className='item'>
+              <img src='/img/greencheck.png' alt='greencheck icon' />
+              <span>Artwork delivery</span>
+            </div>
+            <div className='item'>
+              <img src='/img/greencheck.png' alt='greencheck icon' />
+              <span>Image uploading</span>
+            </div>
+            <div className='item'>
+              <img src='/img/greencheck.png' alt='greencheck icon' />
+              <span>Additional design</span>
+            </div>
+          </div>
+          <button>Continue</button>
+        </div>
       </div>
     </main>
   );
