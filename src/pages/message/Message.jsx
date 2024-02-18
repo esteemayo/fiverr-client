d948bcc243dcecdd1a7f8f41b714d79d1435c820
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-18 19:33:02 +0100

    feat: add markup

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -34,6 +34,54 @@ const Message = () => {
               laboriosam nisi animi! Error, tenetur!
             </p>
           </div>
+          <div className='item'>
+            <img
+              src='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/168679800/original/51e1dc11dcae4c2c8b3f6c42e0fbf1d355558625/design-a-stunning-ui-and-build-a-fullstack-flutter-app.jpg'
+              alt='avatar'
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing eleit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className='item owner'>
+            <img
+              src='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/168679800/original/51e1dc11dcae4c2c8b3f6c42e0fbf1d355558625/design-a-stunning-ui-and-build-a-fullstack-flutter-app.jpg'
+              alt='avatar'
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing eleit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className='item'>
+            <img
+              src='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/168679800/original/51e1dc11dcae4c2c8b3f6c42e0fbf1d355558625/design-a-stunning-ui-and-build-a-fullstack-flutter-app.jpg'
+              alt='avatar'
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing eleit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className='item owner'>
+            <img
+              src='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/168679800/original/51e1dc11dcae4c2c8b3f6c42e0fbf1d355558625/design-a-stunning-ui-and-build-a-fullstack-flutter-app.jpg'
+              alt='avatar'
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing eleit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
         </section>
         <section className='write'>
           <textarea
