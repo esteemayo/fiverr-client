b7c83045dc8ea9db7fb8f505aecec03c2f40f7cc
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-18 03:42:22 +0100

    fix: modify

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -99,6 +99,44 @@ const MyGigs = () => {
                 />
               </td>
             </tr>
+            <tr>
+              <td>
+                <img
+                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
+                  alt='image'
+                  className='img'
+                />
+              </td>
+              <td>Gig 1</td>
+              <td>$88</td>
+              <td>123</td>
+              <td>
+                <img
+                  src='/img/delete.png'
+                  alt='delete icon'
+                  className='delete'
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
+                  alt='image'
+                  className='img'
+                />
+              </td>
+              <td>Gig 1</td>
+              <td>$88</td>
+              <td>123</td>
+              <td>
+                <img
+                  src='/img/delete.png'
+                  alt='delete icon'
+                  className='delete'
+                />
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
