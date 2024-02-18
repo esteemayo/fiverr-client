db8cb5896b89795c289b081a5d812e4c901d77e8
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-18 19:04:46 +0100

    feat: add markup

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,7 +1,41 @@
+import { Link } from 'react-router-dom';
+
 import './Message.scss';
 
 const Message = () => {
-  return <div className='message'>Message</div>;
+  return (
+    <main className='message'>
+      <div className='container'>
+        <span className='breadCrumbs'>
+          <Link to='/messages'>MESSAGES</Link> &gt; JOHN DOE &gt;
+        </span>
+        <section className='messages'>
+          <div className='item'>
+            <img
+              src='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/168679800/original/51e1dc11dcae4c2c8b3f6c42e0fbf1d355558625/design-a-stunning-ui-and-build-a-fullstack-flutter-app.jpg'
+              alt='avatar'
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing eleit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+        </section>
+        <section className='write'>
+          <textarea
+            name=''
+            id=''
+            placeholder='write a message'
+            cols='30'
+            rows='10'
+          />
+          <button>Send</button>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Message;
