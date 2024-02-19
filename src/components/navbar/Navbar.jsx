0f0e0c815c8e28f0472db96efbe8062f4cb0e0db
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-19 02:39:30 +0100

    fix: modify

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <span>Fiverr Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Sign in</span>
+          <Link to='/login'>Sign in</Link>
           {!currentUser.isSeller && <span>Become a Seller</span>}
           {!currentUser && <button>Join</button>}
           {!!currentUser && (
