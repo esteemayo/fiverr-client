fcfd29f4a5a11b0a1b0c40ec4abd78a8c92df986
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-19 02:58:48 +0100

    feat: create Star component

---
+++ src/components/star/Star.jsx
@@ -0,0 +1,16 @@
+import './Star.scss';
+
+const Star = () => {
+  return (
+    <div className='stars'>
+      <img src='/img/star.png' alt='star icon' />
+      <img src='/img/star.png' alt='star icon' />
+      <img src='/img/star.png' alt='star icon' />
+      <img src='/img/star.png' alt='star icon' />
+      <img src='/img/star.png' alt='star icon' />
+      <span>5</span>
+    </div>
+  );
+};
+
+export default Star;
