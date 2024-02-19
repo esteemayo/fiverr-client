1bd91dc90923f1c1ac5e405e4e379afc8ed673dc
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-19 02:41:12 +0100

    feat: add styling

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,11 @@
 import './Login.scss';
 
 const Login = () => {
-  return <div className='login'>Login</div>;
+  return (
+    <main className='login'>
+      <div className='container'>Login</div>
+    </main>
+  );
 };
 
 export default Login;
