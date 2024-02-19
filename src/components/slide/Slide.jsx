0acad0616e69c85e0d4ba315b4b0c8e90adf35b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-19 03:56:05 +0100

    feat: add styling

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -2,11 +2,11 @@ import Slider from 'infinite-react-carousel';
 
 import './Slide.scss';
 
-const Slide = ({ arrowsScroll, slidesToShow, children }) => {
+const Slide = ({ arrowsScroll, slidesToShow, className, children }) => {
   return (
     <section className='slide'>
       <div className='container'>
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll} className={className}>
           {children}
         </Slider>
       </div>
