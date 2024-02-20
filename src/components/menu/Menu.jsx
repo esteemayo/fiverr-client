129b850911bfe7e7689a378a15eb53e7d6aca3c3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-20 19:42:53 +0100

    fix: refactor

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,19 +1,20 @@
 import { Link } from 'react-router-dom';
 
+import { menus } from '../../data';
+
 import './Menu.scss';
 
 const Menu = () => {
   return (
     <div className='menu'>
-      <Link to='/'>Graphics & Design</Link>
-      <Link to='/'>Video & Animation</Link>
-      <Link to='/'>Writing & Translation</Link>
-      <Link to='/'>AI Services</Link>
-      <Link to='/'>Digital Marketing</Link>
-      <Link to='/'>Music & Audio</Link>
-      <Link to='/'>Programming & Tech</Link>
-      <Link to='/'>Business</Link>
-      <Link to='/'>Lifestyle</Link>
+      {menus.map((item) => {
+        const { id, label } = item;
+        return (
+          <Link key={id} to='/'>
+            {label}
+          </Link>
+        );
+      })}
     </div>
   );
 };
