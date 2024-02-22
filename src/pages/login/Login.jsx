0937426425289d01089fa09b8c3e603d34ec8ec5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-22 19:17:04 +0100

    feat: add html markup

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,20 @@ import './Login.scss';
 const Login = () => {
   return (
     <main className='login'>
-      <div className='container'>Login</div>
+      <div className='container'>
+        <form >
+          <h1>Sign in</h1>
+          <div className="formGroup">
+            <label htmlFor="username">Username</label>
+            <input type="text" id='username' placeholder='johndoe' />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' placeholder='johndoe@gmail.com' />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </main>
   );
 };
