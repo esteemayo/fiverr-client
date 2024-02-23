79b7fd2c440dbbab0cab8fdeac685b931b0b4198
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-23 02:54:52 +0100

    feat: add setToStorage method

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,12 @@
+export const tokenKey = 'accessToken';
+
 export const excerpts = (str, count) => {
   if (str.length > count) {
     str = str.substring(0, count).concat('...');
   }
   return str;
+};
+
+export const setToStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
 };
