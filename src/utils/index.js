2d0c73b9c585f6ddc575f2c852e9570259efc62c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-23 03:09:06 +0100

    feat: add removeFromStorage method

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,3 +14,7 @@ export const getFromStorage = (key) => {
 export const setToStorage = (key, value) => {
   return localStorage.setItem(key, JSON.stringify(value));
 };
+
+export const removeFromStorage = (key) => {
+  return localStorage.removeItem(key);
+};
