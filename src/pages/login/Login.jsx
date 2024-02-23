45a26eeb6aab848d5371ef38294790292ea84659
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-23 01:31:28 +0100

    feat: add handleSubmit and handleChange methods

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,39 @@
+import { useCallback, useState } from 'react';
+
 import './Login.scss';
 
+const initialState = {
+  username: '',
+  password: '',
+};
+
 const Login = () => {
+  const [data, setData] = useState(initialState);
+  const [error, setError] = useState(null);
+
+  const handleChange = useCallback(({ target: input }) => {
+    const { name, value } = input;
+    setData((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
   return (
     <main className='login'>
       <div className='container'>
         <div className='wrapper'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h1>Sign in</h1>
             <div className='formGroup'>
               <label htmlFor='username'>Username</label>
-              <input type='text' id='username' placeholder='johndoe' />
+              <input
+                type='text'
+                id='username'
+                placeholder='johndoe'
+                onChange={handleChange}
+              />
             </div>
             <div className='formGroup'>
               <label htmlFor='password'>Password</label>
@@ -17,6 +41,7 @@ const Login = () => {
                 type='password'
                 id='password'
                 placeholder='johndoe@gmail.com'
+                onChange={handleChange}
               />
             </div>
             <button type='submit'>Login</button>
