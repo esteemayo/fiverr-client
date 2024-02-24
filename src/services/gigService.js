9042a9c3064ea7af26a0a7ca43aa13cb1466d66e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-25 00:31:38 +0100

    fix: modify

--- src/services/gigService.js
+++ src/services/gigService.js
@@ -2,4 +2,5 @@ import http from './httpService';
 
 const apiEndpoint = '/gigs';
 
-export const getGigs = (search) => http.get(`${apiEndpoint}${search}`);
+export const getGigs = (search, min, max) =>
+  http.get(`${apiEndpoint}${search}&min=${min}&max=${max}`);
