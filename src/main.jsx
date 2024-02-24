e838bef8c8b49953de34e1b503976b8e19ab6cfd
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-24 19:40:18 +0100

    feat: add QueryProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App.jsx'
+import App from './App.jsx';
+import QueryProvider from './providers/QueryProvider.jsx';
 
-import './index.scss'
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryProvider>
+      <App />
+    </QueryProvider>
+  </React.StrictMode>
+);
