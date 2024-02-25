9d3d108a15be0a4c2633f16a91c2dfada95baed4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-25 01:28:59 +0100

    feat: dynamically render component

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,21 +1,56 @@
 import { Link } from 'react-router-dom';
+import { useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { getUser } from '../../services/userService';
+import { formatCurrency } from '../../utils/formatCurrency';
 
 import './GigCard.scss';
 
-const GigCard = ({ id, img, pp, desc, star, price, username }) => {
+const GigCard = ({
+  _id: id,
+  desc,
+  user,
+  cover,
+  price,
+  title,
+  totalStars,
+  starNumber,
+}) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: [`${user}`],
+    queryFn: async () => {
+      const { data } = await getUser(user);
+      return data;
+    },
+  });
+
+  const stars = useMemo(() => {
+    return (
+      !isNaN(Math.round(totalStars / starNumber)) &&
+      Math.round(totalStars / starNumber)
+    );
+  }, [starNumber, totalStars]);
+
   return (
     <Link to={`/gig/${id}`}>
       <article className='gigCard'>
-        <img src={img} alt='image' />
+        <img src={cover} alt={title} />
         <div className='info'>
-          <div className='user'>
-            <img src={pp} alt={username} />
-            <span>{username}</span>
-          </div>
+          {isLoading ? (
+            'loading'
+          ) : error ? (
+            'Something went wrong!'
+          ) : (
+            <div className='user'>
+              <img src={data.img ?? '/img/noavatar.jpg'} alt={data.username} />
+              <span>{data.username}</span>
+            </div>
+          )}
           <p>{desc}</p>
           <div className='star'>
             <img src='/img/star.png' alt='star' />
-            <span>{star}</span>
+            <span>{stars}</span>
           </div>
         </div>
         <hr />
@@ -23,7 +58,7 @@ const GigCard = ({ id, img, pp, desc, star, price, username }) => {
           <img src='/img/heart.png' alt='heart icon' />
           <div className='price'>
             <span>STARTING AT</span>
-            <h2>${price}</h2>
+            <h2>{formatCurrency(price)}</h2>
           </div>
         </div>
       </article>
