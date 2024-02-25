46301c9547e83d1c9f2b36f4fc39bf6d91683ce0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-25 02:19:45 +0100

    feat: fetch gig from the server

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,15 +1,38 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
 import GigInfo from '../../components/gigInfo/GigInfo';
 import GigContent from '../../components/gigContent/GigContent';
+
+import { getGig } from '../../services/gigService';
 
 import './Gig.scss';
 
 const Gig = () => {
+  const { id } = useParams();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['gig'],
+    queryFn: async () => {
+      const { data } = await getGig(id);
+      return data;
+    },
+  });
+
+  console.log(data);
+
   return (
     <main className='gig'>
-      <div className='container'>
-        <GigContent />
-        <GigInfo />
-      </div>
+      {isLoading ? (
+        'loading'
+      ) : error ? (
+        'Something went wrong!'
+      ) : (
+        <div className='container'>
+          <GigContent {...data} />
+          <GigInfo {...data} />
+        </div>
+      )}
     </main>
   );
 };
