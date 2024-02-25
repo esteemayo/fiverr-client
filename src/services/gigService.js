3f64450f12b2a880226d05e070cc343d5b2fa580
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-25 01:57:59 +0100

    fix: refactor

--- src/services/gigService.js
+++ src/services/gigService.js
@@ -2,7 +2,9 @@ import http from './httpService';
 
 const apiEndpoint = '/gigs';
 
+const gigUrl = (gigId) => `${apiEndpoint}/${gigId}`;
+
 export const getGigs = (search, min, max, sort) =>
   http.get(`${apiEndpoint}${search}&min=${min}&max=${max}&sort=${sort}`);
 
-export const getGig = (gigId) => http.get(`${apiEndpoint}/${gigId}`);
+export const getGig = (gigId) => http.get(gigUrl(gigId));
