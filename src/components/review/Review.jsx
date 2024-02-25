891f69269c8b8a4382bd11654d0cf574af32c9e6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-25 03:52:25 +0100

    fix: change classname

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -5,7 +5,7 @@ import './Review.scss';
 const Review = () => {
   return (
     <>
-      <article className='reviewItem'>
+      <article className='review'>
         <div className='user'>
           <img
             src='https://fiverr-res.cloudinary.com/t_profile_original,q_auto,f_auto/attachments/profile/photo/23905bcdab16b427c8612965f318b4e0-1660400198734/8275baf4-be7e-4120-8a7a-ddf0fde94600.png'
