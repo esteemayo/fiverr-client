1f98b89d8df6efe0062f77fc1244fdeb6c31bab4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-25 02:20:00 +0100

    feat: dynamically render component

--- src/components/gigInfo/GigInfo.jsx
+++ src/components/gigInfo/GigInfo.jsx
@@ -1,43 +1,41 @@
+import { formatCurrency } from '../../utils/formatCurrency';
+
 import './GigInfo.scss';
 
-const GigInfo = () => {
+const GigInfo = ({
+  price,
+  features,
+  shortTitle,
+  shortDesc,
+  deliveryTime,
+  revisionNumber,
+}) => {
   return (
     <aside className='gigInfo'>
       <div className='price'>
-        <h3>1 AI generated image</h3>
-        <h2>$59.99</h2>
+        <h3>{shortTitle}</h3>
+        <h2>{formatCurrency(price)}</h2>
       </div>
-      <p>
-        I will create a unique high quality AI generated image based on a
-        description that you give me
-      </p>
+      <p>{shortDesc}</p>
       <div className='details'>
         <div className='item'>
           <img src='/img/clock.png' alt='clock icon' />
-          <span>2 days delivery</span>
+          <span>{deliveryTime} days delivery</span>
         </div>
         <div className='item'>
           <img src='/img/recycle.png' alt='recycle icon' />
-          <span>3 Revisions</span>
+          <span>{revisionNumber} revisions</span>
         </div>
       </div>
       <div className='feature'>
-        <div className='item'>
-          <img src='/img/greencheck.png' alt='greencheck icon' />
-          <span>Prompt writing</span>
-        </div>
-        <div className='item'>
-          <img src='/img/greencheck.png' alt='greencheck icon' />
-          <span>Artwork delivery</span>
-        </div>
-        <div className='item'>
-          <img src='/img/greencheck.png' alt='greencheck icon' />
-          <span>Image uploading</span>
-        </div>
-        <div className='item'>
-          <img src='/img/greencheck.png' alt='greencheck icon' />
-          <span>Additional design</span>
-        </div>
+        {features.map((feature) => {
+          return (
+            <div key={feature} className='item'>
+              <img src='/img/greencheck.png' alt='greencheck icon' />
+              <span>{feature}</span>
+            </div>
+          );
+        })}
       </div>
       <button>Continue</button>
     </aside>
