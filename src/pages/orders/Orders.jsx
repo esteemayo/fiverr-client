4c2dba9d71499f5e0362562e14e62cd0023e82b3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-26 15:42:55 +0100

    feat: dynamically render component

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,152 +1,63 @@
-import { useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { formatCurrency } from '../../utils/formatCurrency';
+import { getUserOrders } from '../../services/orderService';
 
 import './Orders.scss';
 
 const Orders = () => {
-  const currentUser = {
-    id: 1,
-    name: 'John Doe',
-    isSeller: true,
-  };
-
-  const label = useMemo(() => {
-    return currentUser.isSeller ? 'Buyer' : 'Seller';
-  }, [currentUser]);
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['orders'],
+    queryFn: async () => {
+      const { data } = await getUserOrders();
+      return data;
+    },
+  });
 
   return (
     <main className='orders'>
-      <div className='container'>
-        <div className='title'>
-          <h1>Orders</h1>
+      {isLoading ? (
+        'loading'
+      ) : error ? (
+        'Something went wrong!'
+      ) : (
+        <div className='container'>
+          <div className='title'>
+            <h1>Orders</h1>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Contact</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((order) => {
+                const { _id: id, img, title, price } = order;
+                return (
+                  <tr key={id}>
+                    <td>
+                      <img src={img} alt={title} className='img' />
+                    </td>
+                    <td>{title}</td>
+                    <td>{formatCurrency(price)}</td>
+                    <td>
+                      <img
+                        src='/img/message.png'
+                        alt='message icon'
+                        className='message'
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>{label}</th>
-              <th>Contact</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/message.png'
-                  alt='message icon'
-                  className='message'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/message.png'
-                  alt='message icon'
-                  className='message'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/message.png'
-                  alt='message icon'
-                  className='message'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/message.png'
-                  alt='message icon'
-                  className='message'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/message.png'
-                  alt='message icon'
-                  className='message'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/message.png'
-                  alt='message icon'
-                  className='message'
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      )}
     </main>
   );
 };
