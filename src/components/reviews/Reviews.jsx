1d3581920c7dce1a2173579fe9968861bcccc89b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-26 01:49:42 +0100

    feat: fetch reviews for a gig from the server

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,14 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+
 import Review from '../review/Review';
+import { getReviews } from '../../services/reviewService';
 
 import './Reviews.scss';
 
-const Reviews = () => {
+const Reviews = ({ gigId }) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['reviews'],
+    queryFn: async () => {
+      const { data } = await getReviews(gigId);
+      return data;
+    },
+  });
+
   return (
     <section className='reviews'>
       <h2>Reviews</h2>
-      <Review />
-      <Review />
-      <Review />
+      {isLoading
+        ? 'loading'
+        : error
+        ? 'Something went wrong!'
+        : data.map((review) => {
+            return <Review key={review._id} {...review} />;
+          })}
     </section>
   );
 };
