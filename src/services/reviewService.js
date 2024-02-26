6973336745601961cd9540720e32d07a9077e7f0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-26 02:23:43 +0100

    feat: add endpoint to create a new review on a gig

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -3,3 +3,5 @@ import http from './httpService';
 const apiEndpoint = '/reviews';
 
 export const getReviews = (gigId) => http.get(`${apiEndpoint}/gigs/${gigId}`);
+
+export const createReview = (data) => http.post(apiEndpoint, data);
