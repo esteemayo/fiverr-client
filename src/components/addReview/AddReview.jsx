142e8fd84fe1ae904c395c0a6ed765ce7bb4241a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-26 02:45:06 +0100

    feat: handle create review functionality

--- src/components/addReview/AddReview.jsx
+++ src/components/addReview/AddReview.jsx
@@ -1,11 +1,48 @@
+import { useCallback } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { createReview } from '../../services/reviewService';
+
 import './AddReview.scss';
 
-const AddReview = () => {
+const AddReview = ({ gigId }) => {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: async (review) => {
+      const { data } = await createReview(review);
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['reviews'] });
+    },
+  });
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const desc = e.target[0].value;
+      const star = +e.target[1].value;
+
+      const review = {
+        desc,
+        star,
+        gig: gigId,
+      };
+
+      mutate(review);
+
+      e.target.reset();
+    },
+    [mutate]
+  );
+
   return (
     <div className='addReview'>
       <h3>Add a review</h3>
-      <form>
-        <input type='text' name='' id='' placeholder='write your opinion' />
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='write your opinion' />
         <select name='' id=''>
           <option value={1}>1</option>
           <option value={2}>2</option>
