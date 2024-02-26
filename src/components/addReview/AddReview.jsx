1d8b73c3ea694cfe14859ea7112c63d07959f5f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-26 02:15:21 +0100

    feat: add html markup

--- src/components/addReview/AddReview.jsx
+++ src/components/addReview/AddReview.jsx
@@ -1,7 +1,22 @@
 import './AddReview.scss';
 
 const AddReview = () => {
-  return <div className='addReview'>AddReview</div>;
+  return (
+    <div className='addReview'>
+      <h3>Add a review</h3>
+      <form>
+        <input type='text' name='' id='' placeholder='write your opinion' />
+        <select name='' id=''>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+        <button type='submit'>Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddReview;
