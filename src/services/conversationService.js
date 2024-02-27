e60c80ac77f876a3bb2faba9d95606424a797cc7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-27 01:54:37 +0100

    feat: add create conversation api route endpoint

--- src/services/conversationService.js
+++ src/services/conversationService.js
@@ -3,3 +3,6 @@ import http from './httpService';
 const apiEndpoint = '/conversations';
 
 export const getConversations = () => http.get(apiEndpoint);
+
+export const createConversation = (conversation) =>
+  http.post(apiEndpoint, conversation);
