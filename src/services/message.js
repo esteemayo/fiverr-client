f31d08bca36a3ca3185b5164fc5aad0e4c1a0bf3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-27 04:50:20 +0100

    fix: rename file

--- src/services/message.js
+++
@@ -1,8 +0,0 @@
-import http from './httpService';
-
-const apiEndpoint = '/messages';
-
-export const getMessages = (conversationId) =>
-  http.get(`${apiEndpoint}/${conversationId}`);
-
-export const createMessage = (message) => http.post(apiEndpoint, message);
