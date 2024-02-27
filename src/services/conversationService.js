3650a9aa061dd245dc88ec64ca339b9f239cbbf7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-27 04:52:48 +0100

    fix: refactor

--- src/services/conversationService.js
+++ src/services/conversationService.js
@@ -2,10 +2,12 @@ import http from './httpService';
 
 const apiEndpoint = '/conversations';
 
+const conversationUrl = (conversationId) => `${apiEndpoint}/${conversationId}`;
+
 export const getConversations = () => http.get(apiEndpoint);
 
 export const getConversation = (conversationId) =>
-  http.get(`${apiEndpoint}/${conversationId}`);
+  http.get(conversationUrl(conversationId));
 
 export const updateConversation = (conversationId) =>
-  http.patch(`${apiEndpoint}/${conversationId}`);
+  http.patch(conversationUrl(conversationId));
