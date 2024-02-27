e5fdf756f2f46939b7886a02886ddfe784dfe9d9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-27 03:12:59 +0100

    feat: add get single conversation api route endpoint

--- src/services/conversationService.js
+++ src/services/conversationService.js
@@ -4,5 +4,7 @@ const apiEndpoint = '/conversations';
 
 export const getConversations = () => http.get(apiEndpoint);
 
+export const getConversation = (id) => http.get(`${apiEndpoint}/${id}`);
+
 export const updateConversation = (conversationId) =>
   http.patch(`${apiEndpoint}/${conversationId}`);
