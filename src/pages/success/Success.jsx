d88549ec4faf32a8c950ffe77f0348547fb07bf1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-28 05:15:02 +0100

    feat: handle confirm order functionality

--- src/pages/success/Success.jsx
+++ src/pages/success/Success.jsx
@@ -1,7 +1,38 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import './Success.scss';
+import { confirmOrder } from '../../services/orderService';
 
 const Success = () => {
-  return <main className='success'>Success</main>;
+  const navigate = useNavigate();
+  const { search } = useLocation();
+
+  const params = new URLSearchParams(search);
+  const payment_intent = params.get('payment_intent');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await confirmOrder({ payment_intent });
+
+        setTimeout(() => {
+          navigate('/orders');
+        }, 5000);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  return (
+    <main className='success'>
+      <div className='container'>
+        Payment successful.You are being redirected to the orders page. Please
+        do not close the page.
+      </div>
+    </main>
+  );
 };
 
 export default Success;
