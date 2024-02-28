98968719890e5964d7f37992588af6bda81422d0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-28 04:53:53 +0100

    feat: add update order server api endpoint

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -6,3 +6,6 @@ export const getUserOrders = () => http.get(`${apiEndpoint}/user`);
 
 export const createOrder = (gigId) =>
   http.post(`${apiEndpoint}/create-payment-intent/${gigId}`);
+
+export const updateOrder = (gigId, paymentIntent) =>
+  http.patch(`${apiEndpoint}/${gigId}`, paymentIntent);
