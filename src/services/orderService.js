d528957d2737921a18ad633bdaa2d87f467eb739
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-28 03:11:05 +0100

    feat: add create order/payment-intent server api endpoint

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -3,3 +3,6 @@ import http from './httpService';
 const apiEndpoint = '/orders';
 
 export const getUserOrders = () => http.get(`${apiEndpoint}/user`);
+
+export const createOrder = (gigId) =>
+  http.post(`${apiEndpoint}/create-payment-intent/${gigId}`);
