b544d1e49536aa1ea33f1f9cb760824c6104e7c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-28 03:19:05 +0100

    feat: handle create order and payment intent functionality

--- src/pages/pay/Pay.jsx
+++ src/pages/pay/Pay.jsx
@@ -1,7 +1,50 @@
+import { loadStripe } from '@stripe/stripe-js';
+import { useEffect, useState } from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { useParams } from 'react-router-dom';
+
+import { createOrder } from '../../services/orderService';
+import CheckoutForm from '../../components/checkoutForm/CheckoutForm';
+
 import './Pay.scss';
 
+const stripePromise = loadStripe(
+  import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY
+);
+
 const Pay = () => {
-  return <main className='pay'>Pay</main>;
+  const { id } = useParams();
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await createOrder(id);
+        setClientSecret(data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <main className='pay'>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </main>
+  );
 };
 
 export default Pay;
