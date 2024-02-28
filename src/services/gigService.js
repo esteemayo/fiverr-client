078fe1fee9cbd040cf6976874eff92870171ca7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-28 20:44:02 +0100

    feat: add delete gig server api endpoint

--- src/services/gigService.js
+++ src/services/gigService.js
@@ -10,3 +10,5 @@ export const getGigs = (search, min, max, sort) =>
 export const getMyGigs = (userId) => http.get(`${apiEndpoint}?user=${userId}`);
 
 export const getGig = (gigId) => http.get(gigUrl(gigId));
+
+export const deleteGig = (gigId) => http.delete(gigUrl(gigId));
