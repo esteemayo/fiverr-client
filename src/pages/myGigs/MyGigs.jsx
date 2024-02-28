1b3b1d6f8cf85045b997eed34b54a0d4f94dc6cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-28 20:41:08 +0100

    feat: fetch current user' gigs

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,12 +1,23 @@
-import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import { useMemo } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
+import { getMyGigs } from '../../services/gigService';
 import { getCurrentUser } from '../../utils/getCurrentUser';
 
 import './MyGigs.scss';
 
 const MyGigs = () => {
   const currentUser = getCurrentUser();
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['myGigs'],
+    queryFn: async () => {
+      const { data } = await getMyGigs(currentUser._id);
+      return data;
+    },
+  });
 
   const headerLabel = useMemo(() => {
     return currentUser.isSeller ? 'Gigs' : 'Orders';
@@ -32,101 +43,6 @@ const MyGigs = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/delete.png'
-                  alt='delete icon'
-                  className='delete'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/delete.png'
-                  alt='delete icon'
-                  className='delete'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/delete.png'
-                  alt='delete icon'
-                  className='delete'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/delete.png'
-                  alt='delete icon'
-                  className='delete'
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src='https://fiverr-res.cloudinary.com/images/t_small_thumbnail,q_auto,f_auto/gigs/299984800/original/1adee721799ebf42432d37f152c4bf5c9d77d080/angular-web-projects-and-websites.png'
-                  alt='image'
-                  className='img'
-                />
-              </td>
-              <td>Gig 1</td>
-              <td>$88</td>
-              <td>123</td>
-              <td>
-                <img
-                  src='/img/delete.png'
-                  alt='delete icon'
-                  className='delete'
-                />
-              </td>
-            </tr>
             <tr>
               <td>
                 <img
