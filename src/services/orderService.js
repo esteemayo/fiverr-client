b02d7ac87133f3c5e650646b283ff4f41cc3bb03
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-28 05:11:53 +0100

    fix: modify

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -7,5 +7,5 @@ export const getUserOrders = () => http.get(`${apiEndpoint}/user`);
 export const createOrder = (gigId) =>
   http.post(`${apiEndpoint}/create-payment-intent/${gigId}`);
 
-export const updateOrder = (gigId, paymentIntent) =>
-  http.patch(`${apiEndpoint}/${gigId}`, paymentIntent);
+export const confirmOrder = (paymentIntent) =>
+  http.patch(apiEndpoint, paymentIntent);
