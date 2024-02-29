3d6e45f819d8379a1cc1276124d332954fcc3417
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-29 05:57:31 +0100

    fix: rename component

--- src/components/sharedLayout/SharedLayout.jsx
+++ src/components/sharedLayout/SharedLayout.jsx
@@ -3,15 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
 import Footer from '../footer/Footer';
 
-import ToastProvider from '../../providers/ToastProvider';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import ToasterProvider from '../../providers/ToasterProvider';
 
 const SharedLayout = () => {
   return (
     <>
       <Navbar />
       <ErrorBoundary>
-        <ToastProvider />
+        <ToasterProvider />
         <Outlet />
       </ErrorBoundary>
       <Footer />
