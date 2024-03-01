ad5249d9708a963d403be529c173c1ae5a7146d9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-01 03:00:39 +0100

    fix: refactor

--- src/components/trustedBy/TrustedBy.jsx
+++ src/components/trustedBy/TrustedBy.jsx
@@ -1,3 +1,5 @@
+import { trustedBy } from '../../data';
+
 import './TrustedBy.scss';
 
 const TrustedBy = () => {
@@ -5,26 +7,10 @@ const TrustedBy = () => {
     <section className='trustedBy'>
       <div className='container'>
         <span>Trusted by:</span>
-        <img
-          src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook2x.188a797.png'
-          alt='facebook'
-        />
-        <img
-          src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google2x.06d74c8.png'
-          alt='google'
-        />
-        <img
-          src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix2x.887e47e.png'
-          alt='netflix'
-        />
-        <img
-          src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg2x.6dc32e4.png'
-          alt='pandg'
-        />
-        <img
-          src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal2x.22728be.png'
-          alt='paypal'
-        />
+        {trustedBy.map((item) => {
+          const { id, img, desc } = item;
+          return <img key={id} src={img} alt={desc} />;
+        })}
       </div>
     </section>
   );
