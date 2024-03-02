1d7dedbf2fe3a66ed6d51248397338735899da8f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-02 04:42:09 +0100

    fix: modify

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -68,105 +68,103 @@ const Register = () => {
 
   return (
     <main className='register'>
-      <div className='container'>
-        <form onSubmit={handleSubmit}>
-          <div className='left'>
-            <h1>Create a new account</h1>
-            <div className='formGroup'>
-              <label htmlFor='username'>Username</label>
-              <input
-                type='text'
-                id='username'
-                name='username'
-                placeholder='johndoe'
-                onChange={handleChange}
-              />
-            </div>
-            <div className='formGroup'>
-              <label htmlFor='email'>Email</label>
-              <input
-                type='email'
-                id='email'
-                name='email'
-                placeholder='email'
-                onChange={handleChange}
-              />
-            </div>
-            <div className='formGroup'>
-              <label htmlFor='password'>Password</label>
-              <input
-                type='password'
-                id='password'
-                name='password'
-                placeholder='********'
-                onChange={handleChange}
-              />
-            </div>
-            <div className='formGroup'>
-              <label htmlFor='confirmPassword'>Confirm Password</label>
-              <input
-                type='password'
-                id='confirmPassword'
-                name='confirmPassword'
-                placeholder='********'
-                onChange={handleChange}
-              />
-            </div>
-            <div className='formGroup'>
-              <label htmlFor='file'>Profile Picture</label>
-              <input
-                type='file'
-                id='file'
-                accept='image/*'
-                onChange={(e) => setFile(e.target.files[0])}
-              />
-            </div>
-            <div className='formGroup'>
-              <label htmlFor='country'>Country</label>
-              <input
-                type='text'
-                id='country'
-                name='country'
-                placeholder='USA'
-                onChange={handleChange}
-              />
-            </div>
-            <button type='submit'>Register</button>
+      <form onSubmit={handleSubmit}>
+        <div className='left'>
+          <h1>Create a new account</h1>
+          <div className='formGroup'>
+            <label htmlFor='username'>Username</label>
+            <input
+              type='text'
+              id='username'
+              name='username'
+              placeholder='johndoe'
+              onChange={handleChange}
+            />
           </div>
-          <div className='right'>
-            <h1>I want to become a seller</h1>
-            <div className='toggle'>
-              <label htmlFor='isSeller'>Activate the seller account</label>
-              <label htmlFor='' className='switch'>
-                <input type='checkbox' id='isSeller' onChange={handleSeller} />
-                <span className='slider round' />
-              </label>
-            </div>
-            <div className='formGroup'>
-              <label htmlFor='phone'>Phone Number</label>
-              <input
-                type='phone'
-                id='phone'
-                name='phone'
-                placeholder='+1 234 567 89'
-                onChange={handleChange}
-              />
-            </div>
-            <div className='formGroup'>
-              <label htmlFor='desc'>Description</label>
-              <textarea
-                type='text'
-                id='desc'
-                name='desc'
-                cols='30'
-                rows='10'
-                placeholder='A short description of yourself'
-                onChange={handleChange}
-              />
-            </div>
+          <div className='formGroup'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='email'
+              onChange={handleChange}
+            />
           </div>
-        </form>
-      </div>
+          <div className='formGroup'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='********'
+              onChange={handleChange}
+            />
+          </div>
+          <div className='formGroup'>
+            <label htmlFor='confirmPassword'>Confirm Password</label>
+            <input
+              type='password'
+              id='confirmPassword'
+              name='confirmPassword'
+              placeholder='********'
+              onChange={handleChange}
+            />
+          </div>
+          <div className='formGroup'>
+            <label htmlFor='file'>Profile Picture</label>
+            <input
+              type='file'
+              id='file'
+              accept='image/*'
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+          </div>
+          <div className='formGroup'>
+            <label htmlFor='country'>Country</label>
+            <input
+              type='text'
+              id='country'
+              name='country'
+              placeholder='USA'
+              onChange={handleChange}
+            />
+          </div>
+          <button type='submit'>Register</button>
+        </div>
+        <div className='right'>
+          <h1>I want to become a seller</h1>
+          <div className='toggle'>
+            <label htmlFor='isSeller'>Activate the seller account</label>
+            <label htmlFor='' className='switch'>
+              <input type='checkbox' id='isSeller' onChange={handleSeller} />
+              <span className='slider round' />
+            </label>
+          </div>
+          <div className='formGroup'>
+            <label htmlFor='phone'>Phone Number</label>
+            <input
+              type='phone'
+              id='phone'
+              name='phone'
+              placeholder='+1 234 567 89'
+              onChange={handleChange}
+            />
+          </div>
+          <div className='formGroup'>
+            <label htmlFor='desc'>Description</label>
+            <textarea
+              type='text'
+              id='desc'
+              name='desc'
+              cols='30'
+              rows='10'
+              placeholder='A short description of yourself'
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      </form>
     </main>
   );
 };
