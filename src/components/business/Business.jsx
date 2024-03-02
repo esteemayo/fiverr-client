7e26ef915272450b74ba3c1c3602752e03ab7400
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-02 05:44:45 +0100

    fix: refactor

--- src/components/business/Business.jsx
+++ src/components/business/Business.jsx
@@ -27,10 +27,7 @@ const Business = () => {
           <button>Explore Fiverr Business</button>
         </div>
         <div className='item'>
-          <img
-            src='https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png'
-            alt='image'
-          />
+          <img src='/img/business.webp' alt='image' />
         </div>
       </div>
     </section>
