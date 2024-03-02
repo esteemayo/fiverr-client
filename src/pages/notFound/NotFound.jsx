99aef9ed3f84379b9dd11b1e0a9e03d53160ce16
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-02 05:54:55 +0100

    feat: add html markup

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -4,7 +4,9 @@ const NotFound = () => {
   return (
     <main className='notFound'>
       <div className='container'>
-        <div className='wrapper'>NotFound</div>
+        <div className='wrapper'>
+          <img src='/svg/back_home.svg' alt='404' />
+        </div>
       </div>
     </main>
   );
